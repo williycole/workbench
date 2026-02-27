a01f3cc0fe63831dfff7c1dc32767975d0777b9a
William Cole Boren <williycole@users.noreply.github.com>
2026-02-27 15:09:41 -0600

    added inital spawn for astroids

--- love2d/astroids/main.lua
+++ love2d/astroids/main.lua
@@ -1,27 +1,39 @@
 function love.load()
   GREET = "Hello Astroids!!"
-  Astroid_Radius = love.math.random(10, 100)
-  Astroid_X = love.math.random(10, 100)
-  Astroid_Y = love.math.random(10, 100)
-  Astroid_Base_Count = 5
+
+  local num_astroids = 10
+  Astroids = SpawnAstroids(num_astroids)
 end
 
 function love.draw()
   love.graphics.print(GREET, 0, 0)
-  SpawnAstroids(Astroid_Base_Count)
+  for i = 1, #Astroids do
+    love.graphics.circle("line", Astroids[i].x, Astroids[i].y, Astroids[i].radius)
+  end
 end
 
 function love.update(dt)
-  local Astroid_Speed = 25
-  Astroid_X = Astroid_X + Astroid_Speed * dt
-  Astroid_Y = Astroid_Y + Astroid_Speed * dt
+  for i = 1, #Astroids do
+    Astroids[i].x = Astroids[i].x + Astroids[i].speed * dt
+    Astroids[i].y = Astroids[i].y + Astroids[i].speed * dt
+  end
 end
 
 function SpawnAstroids(num_astroids)
-  local i = 0
-  while i <= num_astroids do
-    i = i + 1
-    love.graphics.circle("line", Astroid_X, Astroid_Y, Astroid_Radius)
-    print("astroid " .. i .. " spawned at " .. Astroid_X .. ", " .. Astroid_Y .. "with radius " .. Astroid_Radius)
+  Astroids = {}
+  for i = 1, num_astroids do
+    Astroids[i] = {
+      x = math.random(0, love.graphics.getWidth()),
+      y = math.random(0, love.graphics.getHeight()),
+      radius = math.random(10, 50),
+      speed = math.random(1, 25)
+    }
+
+    print("Astroid spawned at X: " .. Astroids[i].x
+      .. " Y: " .. Astroids[i].y
+      .. " Radius: " .. Astroids[i].radius
+      .. " Speed: " .. Astroids[i].speed)
   end
+
+  return Astroids
 end
