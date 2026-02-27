c605986d7431b63c9846d178026630cb16ac557c
William Cole Boren <williycole@users.noreply.github.com>
2026-02-27 15:34:18 -0600

    added some files for future org

--- love2d/astroids/main.lua
+++ love2d/astroids/main.lua
@@ -1,22 +1,17 @@
 function love.load()
   GREET = "Hello Astroids!!"
-
-  local num_astroids = 10
-  Astroids = SpawnAstroids(num_astroids)
+  Astroids = SpawnAstroids(10)
+  Ship_x, Ship_y, Ship_angle = 200, 100, math.pi / 4
 end
 
 function love.draw()
-  love.graphics.print(GREET, 0, 0)
-  for i = 1, #Astroids do
-    love.graphics.circle("line", Astroids[i].x, Astroids[i].y, Astroids[i].radius)
-  end
+  DrawAstroids()
+  -- drawShip
+  DrawShip("fill", Ship_x, Ship_y, 20, 10, Ship_angle)
 end
 
-function love.update(dt)
-  for i = 1, #Astroids do
-    Astroids[i].x = Astroids[i].x + Astroids[i].speed * dt
-    Astroids[i].y = Astroids[i].y + Astroids[i].speed * dt
-  end
+function love.update(delta)
+  UpdateAstroidsPosition(delta)
 end
 
 function SpawnAstroids(num_astroids)
@@ -26,7 +21,7 @@ function SpawnAstroids(num_astroids)
       x = math.random(0, love.graphics.getWidth()),
       y = math.random(0, love.graphics.getHeight()),
       radius = math.random(10, 50),
-      speed = math.random(1, 25)
+      speed = math.random(1, 50)
     }
 
     print("Astroid spawned at X: " .. Astroids[i].x
@@ -36,4 +31,26 @@ function SpawnAstroids(num_astroids)
   end
 
   return Astroids
+end
+
+function DrawAstroids()
+  love.graphics.print(GREET, 0, 0)
+  for i = 1, #Astroids do
+    love.graphics.circle("line", Astroids[i].x, Astroids[i].y, Astroids[i].radius)
+  end
+end
+
+function UpdateAstroidsPosition(delta)
+  for i = 1, #Astroids do
+    Astroids[i].x = Astroids[i].x + Astroids[i].speed * delta
+    Astroids[i].y = Astroids[i].y + Astroids[i].speed * delta
+  end
+end
+
+function DrawShip(mode, x, y, length, width, angle) -- position, length, width and angle
+  love.graphics.push()
+  love.graphics.translate(x, y)
+  love.graphics.rotate(angle)
+  love.graphics.polygon(mode, -length / 2, -width / 2, -length / 2, width / 2, length / 2, 0)
+  love.graphics.pop()
 end
