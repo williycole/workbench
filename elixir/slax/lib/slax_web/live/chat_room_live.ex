a0d0e32ce16f2ea91996b6f03d8306effc48c75a
williycole <williycole@users.noreply.github.com>
2026-04-13 13:45:46 -0500

    use patch instead of navigate

--- elixir/slax/lib/slax_web/live/chat_room_live.ex
+++ elixir/slax/lib/slax_web/live/chat_room_live.ex
@@ -61,7 +61,7 @@ defmodule SlaxWeb.ChatRoomLive do
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      href={~p"/rooms/#{@room}"}
+      patch={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
@@ -71,19 +71,23 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
-  def mount(params, _session, socket) do
+  def mount(_params, _session, socket) do
     rooms = Repo.all(Room)
 
+    {:ok, assign(socket, rooms: rooms)}
+  end
+
+  def handle_params(params, _uri, socket) do
     room =
       case Map.fetch(params, "id") do
         {:ok, id} ->
           Repo.get!(Room, id)
 
         :error ->
-          List.first(rooms)
+          List.first(socket.assigns.rooms)
       end
 
-    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
+    {:noreply, assign(socket, room: room, hide_topic?: false)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
