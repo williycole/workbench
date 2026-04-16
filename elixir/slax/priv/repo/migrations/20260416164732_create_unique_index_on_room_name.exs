f11a11d054c4b99f5cad4f52db267ca3e97294c7
williycole <williycole@users.noreply.github.com>
2026-04-16 12:03:09 -0500

    validate room name uniqueness

---
+++ elixir/slax/priv/repo/migrations/20260416164732_create_unique_index_on_room_name.exs
@@ -0,0 +1,8 @@
+defmodule Slax.Repo.Migrations.CreateUniqueIndexOnRoomName do
+  use Ecto.Migration
+
+  def change do
+    create unique_index(:rooms, [:name])
+    # create unique_index(:rooms, :name)
+  end
+end
