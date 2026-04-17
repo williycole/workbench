295c0e6eeb65a5ca6f1d42140da4920a8f8719d5
williycole <williycole@users.noreply.github.com>
2026-04-17 08:59:27 -0500

    mix phx.gen.auth

--- elixir/slax/config/config.exs
+++ elixir/slax/config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :slax, :scopes,
+  user: [
+    default: true,
+    module: Slax.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Slax.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :slax,
   ecto_repos: [Slax.Repo],
   generators: [timestamp_type: :utc_datetime]
--- elixir/slax/lib/slax_web/router.ex
+++ elixir/slax/lib/slax_web/router.ex
@@ -1,6 +1,8 @@
 defmodule SlaxWeb.Router do
   use SlaxWeb, :router
 
+  import SlaxWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule SlaxWeb.Router do
     plug :put_root_layout, html: {SlaxWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -44,5 +47,33 @@ defmodule SlaxWeb.Router do
       live_dashboard "/dashboard", metrics: SlaxWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", SlaxWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{SlaxWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", SlaxWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{SlaxWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
