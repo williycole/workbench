8832307b21f2c7629d0f5046d499ea6894bc85bf
williycole <williycole@users.noreply.github.com>
2026-04-20 14:40:21 -0500

    users can send messages to the chat

--- elixir/slax/lib/slax/chat.ex
+++ elixir/slax/lib/slax/chat.ex
@@ -5,6 +5,19 @@ defmodule Slax.Chat do
 
   import Ecto.Query
 
+  def change_message(message, attrs \\ %{}, scope) do
+    Message.changeset(message, attrs, scope)
+  end
+
+  def create_message(room, attrs, scope) do
+    with {:ok, message} <-
+           %Message{room: room}
+           |> Message.changeset(attrs, scope)
+           |> Repo.insert() do
+      {:ok, Repo.preload(message, :user)}
+    end
+  end
+
   def list_messages_in_room(%Room{id: room_id}) do
     Message
     |> where([m], m.room_id == ^room_id)
