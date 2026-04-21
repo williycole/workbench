efcc2689b2726d91de33692a6eed8d68f4783538
williycole <williycole@users.noreply.github.com>
2026-04-21 14:00:00 -0500

    only show delete button on hover

--- elixir/slax/lib/slax_web/live/chat_room_live.ex
+++ elixir/slax/lib/slax_web/live/chat_room_live.ex
@@ -107,10 +107,10 @@ defmodule SlaxWeb.ChatRoomLive do
 
   defp message(assigns) do
     ~H"""
-    <div id={@dom_id} class="relative flex px-4 py-3">
+    <div id={@dom_id} class="group relative flex px-4 py-3">
       <button
         :if={@current_user.id == @message.user_id}
-        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer"
+        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer hidden group-hover:block"
         data-confirm="Are you sure?"
         phx-click="delete-message"
         phx-value-id={@message.id}
