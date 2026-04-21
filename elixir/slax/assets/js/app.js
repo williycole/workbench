51efdff2c8a5b78429b2aa44f739434a88ff1ad8
williycole <williycole@users.noreply.github.com>
2026-04-21 11:45:13 -0500

    show timestamps in correct timezone

--- elixir/slax/assets/js/app.js
+++ elixir/slax/assets/js/app.js
@@ -20,20 +20,23 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import {hooks as colocatedHooks} from "phoenix-colocated/slax"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
+import { hooks as colocatedHooks } from "phoenix-colocated/slax"
 import topbar from "../vendor/topbar"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
-  hooks: {...colocatedHooks},
+  params: {
+    _csrf_token: csrfToken,
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  },
+  hooks: { ...colocatedHooks },
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
@@ -53,7 +56,7 @@ window.liveSocket = liveSocket
 //     2. click on elements to jump to their definitions in your code editor
 //
 if (process.env.NODE_ENV === "development") {
-  window.addEventListener("phx:live_reload:attached", ({detail: reloader}) => {
+  window.addEventListener("phx:live_reload:attached", ({ detail: reloader }) => {
     // Enable server log streaming to client.
     // Disable with reloader.disableServerLogs()
     reloader.enableServerLogs()
@@ -66,11 +69,11 @@ if (process.env.NODE_ENV === "development") {
     window.addEventListener("keydown", e => keyDown = e.key)
     window.addEventListener("keyup", _e => keyDown = null)
     window.addEventListener("click", e => {
-      if(keyDown === "c"){
+      if (keyDown === "c") {
         e.preventDefault()
         e.stopImmediatePropagation()
         reloader.openEditorAtCaller(e.target)
-      } else if(keyDown === "d"){
+      } else if (keyDown === "d") {
         e.preventDefault()
         e.stopImmediatePropagation()
         reloader.openEditorAtDef(e.target)
--- elixir/slax/lib/slax_web/live/chat_room_live.ex
+++ elixir/slax/lib/slax_web/live/chat_room_live.ex
@@ -64,7 +64,12 @@ defmodule SlaxWeb.ChatRoomLive do
         </div>
 
         <div id="room-messages" class="flex flex-col grow overflow-auto" phx-update="stream">
-          <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
+          <.message
+            :for={{dom_id, message} <- @streams.messages}
+            dom_id={dom_id}
+            message={message}
+            timezone={@timezone}
+          />
         </div>
 
         <div class="bg-white px-4">
@@ -96,6 +101,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
+  attr :timezone, :string, required: true
 
   defp message(assigns) do
     ~H"""
@@ -106,7 +112,9 @@ defmodule SlaxWeb.ChatRoomLive do
           <.link class="text-sm font-semibold hover:underline">
             <span>{username(@message.user)}</span>
           </.link>
-          <span class="ml-1 text-xs text-gray-500">{message_timestamp(@message)}</span>
+          <span :if={@timezone} class="ml-1 text-xs text-gray-500">
+            {message_timestamp(@message, @timezone)}
+          </span>
           <p class="text-sm">{@message.body}</p>
         </div>
       </div>
@@ -118,8 +126,9 @@ defmodule SlaxWeb.ChatRoomLive do
     user.email |> String.split("@") |> List.first() |> String.capitalize()
   end
 
-  defp message_timestamp(message) do
+  defp message_timestamp(message, timezone) do
     message.inserted_at
+    |> Timex.Timezone.convert(timezone)
     |> Timex.format!("%-l:%M %p", :strftime)
   end
 
@@ -145,8 +154,9 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
+    timezone = get_connect_params(socket)["timezone"]
 
-    {:ok, assign(socket, rooms: rooms)}
+    {:ok, assign(socket, rooms: rooms, timezone: timezone)}
   end
 
   def handle_params(params, _uri, socket) do
