f92880736385183f6d02cd2326a3ded518bf048b
williycole <williycole@users.noreply.github.com>
2026-08-04 11:28:01 -0500

    backup while working on format confusion

--- elixir/exercism/elixir/high-score/lib/high_score.ex
+++ elixir/exercism/elixir/high-score/lib/high_score.ex
@@ -1,25 +1,31 @@
 defmodule HighScore do
+  @initial_score 0
+
   def new() do
-    # Please implement the new/0 function
+    %{}
   end
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  def add_player(scores, name, score \\ @initial_score) do
+    Map.put(scores, name, score)
   end
 
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    Map.delete(scores, name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    Map.put(scores, name, @initial_score)
   end
 
   def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+    Map.update(scores, name, score, fn s -> score + s end)
+  end
+
+  def get_players(nil) do
+    nil
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    Map.keys(scores)
   end
 end
