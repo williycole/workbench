3a3dd2de213255246450e11cdd3289a3b9506fbc
williycole <williycole@users.noreply.github.com>
2026-08-11 12:40:32 -0500

    backup

--- elixir/exercism/elixir/name-badge/lib/name_badge.ex
+++ elixir/exercism/elixir/name-badge/lib/name_badge.ex
@@ -1,5 +1,18 @@
 defmodule NameBadge do
+  # def print(nil, name, nil),
+  #   do: "#{name} - OWNER"
+  #
+  # def print(id, name, nil),
+  #   do: "[#{id}] - #{name} - OWNER"
+  #
+  # def print(nil, name, department),
+  #   do: "#{name} - #{String.upcase(department)}"
+  #
+  # def print(id, name, department),
+  #   do: "[#{id}] - #{name} - #{String.upcase(department)}"
   def print(id, name, department) do
-    # Please implement the print/3 function
+    prefix = if id, do: "[#{id}] - ", else: ""
+    dept = if department, do: String.upcase(department), else: "OWNER"
+    "#{prefix}#{name} - #{dept}"
   end
 end
